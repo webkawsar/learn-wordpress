52355dfe7d1dcd640dd843240323559ac6506cd0
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-06-27 23:33:31 +0600

    index page dynamic link to assets folder

--- class-16/wp-content/themes/corporate/index.php
+++ class-16/wp-content/themes/corporate/index.php
@@ -19,26 +19,34 @@
 		<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/bootstrap.min.css"?>">
+
 		<!-- Nice Select CSS -->
-		<link rel="stylesheet" href="css/nice-select.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/nice-select.css"?>">
+
 		<!-- Font Awesome CSS -->
-        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/font-awesome.min.css"?>">
+
 		<!-- icofont CSS -->
-        <link rel="stylesheet" href="css/icofont.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/icofont.css"?>">
+
 		<!-- Slicknav -->
-		<link rel="stylesheet" href="css/slicknav.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/slicknav.min.css"?>">
+
 		<!-- Owl Carousel CSS -->
-        <link rel="stylesheet" href="css/owl-carousel.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/owl-carousel.css"?>">
+
 		<!-- Datepicker CSS -->
-		<link rel="stylesheet" href="css/datepicker.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/datepicker.css"?>">
+
 		<!-- Animate CSS -->
-        <link rel="stylesheet" href="css/animate.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/animate.min.css"?>">
+		
 		<!-- Magnific Popup CSS -->
-        <link rel="stylesheet" href="css/magnific-popup.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/magnific-popup.css"?>">
 		
 		<!-- Medipro CSS -->
-        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/normalize.css"?>">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/style.css"?>">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri()."/assets/css/responsive.css"?>">
 		
@@ -1015,44 +1023,63 @@
 		<!--/ End Footer Area -->
 		
 		<!-- jquery Min JS -->
-        <script src="js/jquery.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/jquery.min.js"?>"></script>
+
 		<!-- jquery Migrate JS -->
-		<script src="js/jquery-migrate-3.0.0.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/jquery-migrate-3.0.0.js"?>"></script>
+
 		<!-- jquery Ui JS -->
-		<script src="js/jquery-ui.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/jquery-ui.min.js"?>"></script>
+
 		<!-- Easing JS -->
-        <script src="js/easing.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/easing.js"?>"></script>
+
 		<!-- Color JS -->
-		<script src="js/colors.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/colors.js"?>"></script>
+
 		<!-- Popper JS -->
-		<script src="js/popper.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/popper.min.js"?>"></script>
+
 		<!-- Bootstrap Datepicker JS -->
-		<script src="js/bootstrap-datepicker.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/bootstrap-datepicker.js"?>"></script>
+
 		<!-- Jquery Nav JS -->
-        <script src="js/jquery.nav.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/jquery.nav.js"?>"></script>
+
 		<!-- Slicknav JS -->
-		<script src="js/slicknav.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/slicknav.min.js"?>"></script>
+
 		<!-- ScrollUp JS -->
-        <script src="js/jquery.scrollUp.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/jquery.scrollUp.min.js"?>"></script>
+
 		<!-- Niceselect JS -->
-		<script src="js/niceselect.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/niceselect.js"?>"></script>
+
 		<!-- Tilt Jquery JS -->
-		<script src="js/tilt.jquery.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/tilt.jquery.min.js"?>"></script>
+
 		<!-- Owl Carousel JS -->
-        <script src="js/owl-carousel.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/owl-carousel.js"?>"></script>
+
 		<!-- counterup JS -->
-		<script src="js/jquery.counterup.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/jquery.counterup.min.js"?>"></script>
+
 		<!-- Steller JS -->
-		<script src="js/steller.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/steller.js"?>"></script>
+
 		<!-- Wow JS -->
-		<script src="js/wow.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/wow.min.js"?>"></script>
+
 		<!-- Magnific Popup JS -->
-		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/jquery.magnific-popup.min.js"?>"></script>
+
 		<!-- Counter Up CDN JS -->
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+
 		<!-- Bootstrap JS -->
-		<script src="js/bootstrap.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/bootstrap.min.js"?>"></script>
+
 		<!-- Main JS -->
-		<script src="js/main.js"></script>
+		<script src="<?php echo get_template_directory_uri()."/assets/js/main.js"?>"></script>
     </body>
 </html>
