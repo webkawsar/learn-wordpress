63e10c57f8a83f9741d133c1e99e4bdae5669d35
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-07-05 20:23:12 +0600

    practice show posts by looping

---
+++ classes/class-20/corporate/functions.php
@@ -0,0 +1,97 @@
+<?php
+
+if (!function_exists("callbackFunction")) {
+    function callbackFunction()
+    {
+        add_theme_support("post-thumbnails");
+        add_theme_support('post-formats', array('aside', 'gallery', 'quote', 'image', 'video'));
+
+        // translate language
+        load_theme_textdomain("myTranslateId", get_template_directory() . "/languages");
+
+    }
+}
+
+add_action("after_setup_theme", "callbackFunction");
+
+
+
+if (!function_exists('myCustomCSSAndJSS')) {
+    function myCustomCSSAndJSS()
+    {
+        // Google Fonts
+        wp_enqueue_style(
+            'google_fonts',
+            'https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
+            array(),
+            null
+        );
+
+        // <!-- Bootstrap CSS -->
+        wp_enqueue_style('bootstrap', get_template_directory_uri() . "/assets/css/bootstrap.min.css");
+
+        // <!-- Nice Select CSS -->
+        wp_enqueue_style('nice_select', get_template_directory_uri() . "/assets/css/nice-select.css");
+
+        // <!-- Font Awesome CSS -->
+        wp_enqueue_style('font_awesome', get_template_directory_uri() . "/assets/css/font-awesome.min.css");
+
+        // <!-- icofont CSS -->
+        wp_enqueue_style('icofont', get_template_directory_uri() . "/assets/css/icofont.css");
+
+        // <!-- Slicknav -->
+        wp_enqueue_style('slicknav', get_template_directory_uri() . "/assets/css/slicknav.min.css");
+
+        // <!-- Owl Carousel CSS -->
+        wp_enqueue_style('carousel', get_template_directory_uri() . "/assets/css/owl-carousel.css");
+
+        // <!-- Datepicker CSS -->
+        wp_enqueue_style('datepicker', get_template_directory_uri() . "/assets/css/datepicker.css");
+
+        // <!-- Animate CSS -->
+        wp_enqueue_style('animate', get_template_directory_uri() . "/assets/css/animate.min.css");
+
+        // <!-- Magnific Popup CSS -->
+        wp_enqueue_style('magnific', get_template_directory_uri() . "/assets/css/magnific-popup.css");
+
+        // <!-- Medipro CSS -->
+        wp_enqueue_style('normalize', get_template_directory_uri() . "/assets/css/normalize.css");
+        wp_enqueue_style('style', get_template_directory_uri() . "/assets/style.css");
+        wp_enqueue_style('responsive', get_template_directory_uri() . "/assets/css/responsive.css");
+
+    }
+}
+
+add_action('wp_enqueue_scripts', 'myCustomCSSAndJSS');
+
+
+if(!function_exists("my_menu")) {
+    function my_menu() {
+        register_nav_menus(array(
+            "top_menu" => __("Top Menu", "myTranslateId"),
+            "primary_menu" => __("Primary Menu", "myTranslateId"),
+        ));
+    }
+}
+
+add_action('init', 'my_menu');
+
+
+
+// Add 'dropdown' class to sub-menu <ul>
+function my_add_dropdown_class($classes) {
+    $classes[] = 'dropdown';
+    return $classes;
+}
+add_filter('nav_menu_submenu_css_class', 'my_add_dropdown_class');
+
+// Add icon to menu items with children
+function my_add_menu_icon($title, $item, $args, $depth) {
+    if (in_array('menu-item-has-children', $item->classes) && $depth === 0) {
+        $title .= ' <i class="icofont-rounded-down"></i>';
+    }
+    return $title;
+}
+add_filter('nav_menu_item_title', 'my_add_menu_icon', 10, 4);
+
+?>
---
+++ classes/class-20/corporate/template-parts/header_area.php
@@ -0,0 +1,95 @@
+<header class="header">
+    <!-- Topbar -->
+    <div class="topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-5 col-12">
+                    <!-- Contact -->
+                    <?php
+                        wp_nav_menu(array(
+                            "theme_location" => "top_menu",
+                            "menu_class" => "top-link"
+                        ));
+                    ?>
+                    <!-- End Contact -->
+                </div>
+
+                <div class="col-lg-6 col-md-7 col-12">
+                    <!-- Top Contact -->
+                    <ul class="top-contact">
+                        <li><i class="fa fa-phone"></i>+880 1234 56789</li>
+                        <li><i class="fa fa-envelope"></i><a href="mailto:support@yourmail.com">support@yourmail.com</a>
+                        </li>
+                    </ul>
+                    <!-- End Top Contact -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Topbar -->
+    <!-- Header Inner -->
+    <div class="header-inner">
+        <div class="container">
+            <div class="inner">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-12">
+                        <!-- Start Logo -->
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="<?php echo get_template_directory_uri() . "/assets/img/logo.png" ?>" alt="#">
+                            </a>
+                        </div>
+                        <!-- End Logo -->
+                        <!-- Mobile Nav -->
+                        <div class="mobile-nav"></div>
+                        <!-- End Mobile Nav -->
+                    </div>
+                    <div class="col-lg-7 col-md-9 col-12">
+                        <!-- Main Menu -->
+                        <div class="main-menu">
+                            <nav class="navigation">
+                                <!-- <ul class="nav menu">
+                                    <li class="active">
+                                        <a href="#">Home <i class="icofont-rounded-down"></i></a>
+                                        <ul class="dropdown">
+                                            <li><a href="index.html">Home Page 1</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Doctors </a></li>
+                                    <li><a href="#">Services </a></li>
+                                    <li><a href="#">Pages <i class="icofont-rounded-down"></i></a>
+                                        <ul class="dropdown">
+                                            <li><a href="404.html">404 Error</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Blogs <i class="icofont-rounded-down"></i></a>
+                                        <ul class="dropdown">
+                                            <li><a href="blog-single.html">Blog Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                </ul> -->
+
+                                <?php
+                                    wp_nav_menu(array(
+                                        "theme_location" => "primary_menu",
+                                        "menu_class" => "nav menu",
+                                        'container'      => false,
+                                    ));
+                                ?>
+                                
+                            </nav>
+                        </div>
+                        <!--/ End Main Menu -->
+                    </div>
+                    <div class="col-lg-2 col-12">
+                        <div class="get-quote">
+                            <a href="appointment.html" class="btn">Book Appointment</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ End Header Inner -->
+</header>
