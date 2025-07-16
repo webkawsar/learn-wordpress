5d9e2366ea34c0988f92256ef633ad1f8bb46bc5
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-07-16 22:33:51 +0600

    added class 24 code to folder

---
+++ classes/class-24/corporate/functions.php
@@ -0,0 +1,176 @@
+<?php
+
+if (!function_exists("callbackFunction")) {
+    function callbackFunction()
+    {
+        add_theme_support("post-thumbnails");
+        add_theme_support('post-formats', array('aside', 'gallery', 'quote', 'image', 'video'));
+
+        // translate language
+        load_theme_textdomain("myTranslateId", get_template_directory() . "/languages");
+
+    }
+}
+
+add_action("after_setup_theme", "callbackFunction");
+
+
+
+if (!function_exists('myCustomCSSAndJSS')) {
+    function myCustomCSSAndJSS()
+    {
+        // Google Fonts
+        wp_enqueue_style(
+            'google_fonts',
+            'https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
+            array(),
+            null
+        );
+
+        // <!-- Bootstrap CSS -->
+        wp_enqueue_style('bootstrap', get_template_directory_uri() . "/assets/css/bootstrap.min.css");
+
+        // <!-- Nice Select CSS -->
+        wp_enqueue_style('nice_select', get_template_directory_uri() . "/assets/css/nice-select.css");
+
+        // <!-- Font Awesome CSS -->
+        wp_enqueue_style('font_awesome', get_template_directory_uri() . "/assets/css/font-awesome.min.css");
+
+        // <!-- icofont CSS -->
+        wp_enqueue_style('icofont', get_template_directory_uri() . "/assets/css/icofont.css");
+
+        // <!-- Slicknav -->
+        wp_enqueue_style('slicknav', get_template_directory_uri() . "/assets/css/slicknav.min.css");
+
+        // <!-- Owl Carousel CSS -->
+        wp_enqueue_style('carousel', get_template_directory_uri() . "/assets/css/owl-carousel.css");
+
+        // <!-- Datepicker CSS -->
+        wp_enqueue_style('datepicker', get_template_directory_uri() . "/assets/css/datepicker.css");
+
+        // <!-- Animate CSS -->
+        wp_enqueue_style('animate', get_template_directory_uri() . "/assets/css/animate.min.css");
+
+        // <!-- Magnific Popup CSS -->
+        wp_enqueue_style('magnific', get_template_directory_uri() . "/assets/css/magnific-popup.css");
+
+        // <!-- Medipro CSS -->
+        wp_enqueue_style('normalize', get_template_directory_uri() . "/assets/css/normalize.css");
+        wp_enqueue_style('style', get_template_directory_uri() . "/assets/style.css");
+        wp_enqueue_style('responsive', get_template_directory_uri() . "/assets/css/responsive.css");
+
+    }
+}
+
+add_action('wp_enqueue_scripts', 'myCustomCSSAndJSS');
+
+
+if (!function_exists("my_menu")) {
+    function my_menu()
+    {
+        register_nav_menus(array(
+            "top_menu" => __("Top Menu", "myTranslateId"),
+            "primary_menu" => __("Primary Menu", "myTranslateId"),
+        ));
+    }
+}
+
+add_action('init', 'my_menu');
+
+
+
+// Add 'dropdown' class to sub-menu <ul>
+function my_add_dropdown_class($classes)
+{
+    $classes[] = 'dropdown';
+    return $classes;
+}
+add_filter('nav_menu_submenu_css_class', 'my_add_dropdown_class');
+
+// Add icon to menu items with children
+function my_add_menu_icon($title, $item, $args, $depth)
+{
+    if (in_array('menu-item-has-children', $item->classes) && $depth === 0) {
+        $title .= ' <i class="icofont-rounded-down"></i>';
+    }
+    return $title;
+}
+add_filter('nav_menu_item_title', 'my_add_menu_icon', 10, 4);
+
+
+
+if (!function_exists('custom_post')) {
+    function custom_post()
+    {
+        register_post_type("team", array(
+            "labels" => array(
+                "name" => __("BD Team", "myTranslateId"),
+                "singular_name" => __("team", "myTranslateId"),
+            ),
+            "public" => true,
+            "has_archive" => true,
+        ));
+
+
+        register_post_type("service", array(
+            "labels" => array(
+                "name" => __("Services", "myTranslateId"),
+                "singular_name" => __("service", "myTranslateId"),
+                "add_new_item" => __("Add New Service", "myTranslateId"),
+                "add_new" => __("Add New", "myTranslateId"),
+            ),
+            "public" => true,
+            "has_archive" => true,
+            "supports" => array("title", "editor", "thumbnail")
+        ));
+
+
+        register_post_type(
+            'wporg_product',
+            array(
+                'labels' => array(
+                    'name' => __('Products', 'myTranslateId'),
+                    'singular_name' => __('Product', 'myTranslateId'),
+
+                    // এটি override করে labels['name'] এর ভ্যালু। যদি menu_name না দাও, তাহলে name (যেটা 'Products' hobe
+                    // 'menu_name'             => __( 'Post Types', 'myTranslateId' ),
+
+                    // WordPress-এর উপরের admin bar-এ, যখন তুমি "New" মেনুতে ক্লিক করো (যেখানে আছে "New Post", "New Page" ইত্যাদি), তখন তোমার কাস্টম পোস্ট টাইপের জন্য দেখাবে
+                    // 'name_admin_bar' => __('Product', 'myTranslateId'),
+
+                    // এর মানে হলো, যেখানে তোমার কাস্টম পোস্ট টাইপের সবগুলো পোস্ট একসাথে দেখা যাবে (যেমন yoursite.com/products), সেই আর্কাইভ পেজের জন্য ব্যবহৃত টেক্সট হবে "Item Archives"।
+                    // 'archives'              => __( 'Item Archives', 'myTranslateId' ),
+
+                    // 'attributes'            => __( 'Item Attributes', 'myTranslateId' ),
+                    // 'parent_item_colon'     => __( 'Parent Item:', 'myTranslateId' ),
+                    'all_items' => __('All Products', 'myTranslateId'),
+                    'add_new_item' => __('Add Product', 'myTranslateId'),
+                    // 'add_new'               => __( 'নতুন যোগ করুন', 'myTranslateId' ),
+                    // 'new_item'              => __( 'New Item in UI', 'myTranslateId' ),
+
+                    // edit page er heading
+                    'edit_item' => __('Edit Product', 'myTranslateId'),
+                    'search_items' => __('Search Product', 'myTranslateId'),
+                    'not_found' => __('Not found in Products', 'myTranslateId'),
+                    'not_found_in_trash' => __('Not found in Trash', 'myTranslateId'),
+                ),
+                'public' => true,
+                'has_archive' => true,
+                'rewrite' => array('slug' => 'products'), // my custom slug
+                "supports" => array("title", "editor", "thumbnail"),
+            )
+        );
+
+
+    }
+
+}
+
+
+add_action("init", "custom_post");
+
+
+require_once get_theme_file_path("inc/add_employee.php");
+require_once get_theme_file_path("inc/employee_cat.php");
+
+    ?>
